8433f90bd291300a0f2f7f942eb5720425b72471
ntkhaiuet <ntkhaiuet@gmail.com>
2022-05-25 20:05:00 +0700

    add win10-4-8-64 and ubuntu4-8-64

--- toolCreateVM.py
+++ toolCreateVM.py
@@ -4,8 +4,10 @@ import os
 layout = [  [sg.Text('Select VM configuration', font=('Arial',15, 'bold'))],
             [sg.Radio('Windows 10, 2 CPU, 4GB RAM, 32GB Disk', 1, enable_events=True, font=('Arial',13), key='-R1-')],
             [sg.Radio('Windows 10, 2 CPU, 6GB RAM, 50GB Disk',1, enable_events=True, font=('Arial',13), key='-R2-')],
-            [sg.Radio('Ubuntu, 1 CPU, 2GB RAM, 16GB Disk',1, enable_events=True, font=('Arial',13), key='-R3-')],
-            [sg.Radio('Ubuntu, 2 CPU, 4GB RAM, 32GB Disk',1, enable_events=True, font=('Arial',13), key='-R4-')],
+            [sg.Radio('Windows 10, 4 CPU, 8GB RAM, 64GB Disk',1, enable_events=True, font=('Arial',13), key='-R3-')],
+            [sg.Radio('Ubuntu, 1 CPU, 2GB RAM, 16GB Disk',1, enable_events=True, font=('Arial',13), key='-R4-')],
+            [sg.Radio('Ubuntu, 2 CPU, 4GB RAM, 32GB Disk',1, enable_events=True, font=('Arial',13), key='-R5-')],
+            [sg.Radio('Ubuntu, 4 CPU, 8GB RAM, 64GB Disk',1, enable_events=True, font=('Arial',13), key='-R6-')],
             [sg.Text('Path to VMware OVF Tool:', font=('Arial', 15, 'bold'))],
             [sg.In( visible=False), sg.Input(key='-PATH-', font=('Arial', 13), default_text='C:\Program Files\VMware\VMware OVF Tool'), sg.FileBrowse('Browse', target='-DIR-', font=('Arial', 10, 'bold'))],
             [sg.Text('Datastore', font=('Arial',15, 'bold'))],
@@ -32,15 +34,19 @@ while True:             # Event Loop
 
     win10_2_4_32 = values["-R1-"]
     win10_2_6_50 = values["-R2-"]
-    u1_2_16 = values["-R3-"]
-    u2_4_32 = values["-R4-"]
+    win10_4_8_64 = values["-R3-"]
+    u1_2_16 = values["-R4-"]
+    u2_4_32 = values["-R5-"]
+    u4_8_64 = values["-R6-"]
 
     ovaURL = ["https://dl.dropbox.com/s/uurlzcyad172tdu/Windows10-2-4-32.ova",
         "https://dl.dropbox.com/s/2428m49v5uf1jlq/Windows10-2-6-50.ova",
+        "https://dl.dropbox.com/s/0pk33i2prhl8enz/Windows10-4-8-64.ova",
         "https://dl.dropbox.com/s/129ts6r5ojvej2w/Ubuntu1-2-16.ova",
-        "https://dl.dropbox.com/s/rzm8ulufajvpern/Ubuntu2-4-32.ova"]
+        "https://dl.dropbox.com/s/rzm8ulufajvpern/Ubuntu2-4-32.ova",
+        "https://dl.dropbox.com/s/l6td215f7p7u4sy/Ubuntu4-8-64.ova"]
 
-    if (win10_2_4_32 or win10_2_6_50 or u1_2_16 or u2_4_32):
+    if (win10_2_4_32 or win10_2_6_50 or win10_4_8_64 or u1_2_16 or u2_4_32 or u4_8_64):
         if event == 'CREATE' and required1 and required2 and required3 and required4:
             # print(event, values)
 
@@ -54,10 +60,14 @@ while True:             # Event Loop
                 command = command + ovaURL[0] + '" vi://'
             elif win10_2_6_50:
                 command = command + ovaURL[1] + '" vi://'
-            elif u1_2_16:
+            elif win10_4_8_64:
                 command = command + ovaURL[2] + '" vi://'
-            else:
+            elif u1_2_16:
                 command = command + ovaURL[3] + '" vi://'
+            elif u4_8_64:
+                command = command + ovaURL[4] + '" vi://'
+            else:
+                command = command + ovaURL[5] + '" vi://'
             
             username = values["-USERNAME-"]
             password = values["-PASSWORD-"]
